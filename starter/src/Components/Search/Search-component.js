24217cd87cced245a99e050b1d4aa795bf80c083
mahmoudramamdan0040 <mahmoudramamdan0040@gmail.com>
2022-07-13 16:13:39 +0200

    finished

--- starter/src/Components/Search/Search-component.js
+++ starter/src/Components/Search/Search-component.js
@@ -6,27 +6,26 @@ import { useState ,useEffect } from 'react';
 const SearchComponent = () => {
     const [searchBooks,setSearchBooks]=useState([]);
     const [Books,setBooks]=useState([]);
+    const [msgFound,setMsgFound]=useState('');
     useEffect( () => {
         BookApI.getAll().then((books)=>{
-            setSearchBooks(books);
+            // setSearchBooks(books);
             setBooks(books);
             console.log(books);
         });
     },[]);
     const handleSearch = (event) => {
         let query =event.target.value;
-        // console.log(query);
         if(query){
-            // Books.filter();
-            // console.log(query);
             BookApI.search(query).then((book)=>{
+                if(!book.length){
+                    setMsgFound('No books found');
+                }
                 setSearchBooks(book);
             });
-            // console.log(searchBooks);
         }else{
-            BookApI.getAll().then((book)=>{
-                setSearchBooks(book);
-            });
+            setSearchBooks([]);
+            setMsgFound('');
         }
     }
     return ( 
@@ -47,9 +46,7 @@ const SearchComponent = () => {
                     {
                         searchBooks.length ? searchBooks.map((book) => {
                             return <BookComponent key={book.id} book={book} Books={Books} setBooks={setBooks}/>
-                        }): <div>
-                                <h2>No Books Found</h2>
-                            </div>
+                        }): <div>{msgFound}</div>
                     }
                 </ol>
             </div>
